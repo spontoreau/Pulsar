5ec3e34628df75e5b291ecca8dfcecccb43233b6
pontoreau <pontoreau.sylvain@gmail.com>
2014-02-14 21:28:21 +0100

    Edit project and add Assembly information

--- Sfml/Window/Properties/AssemblyInfo.cs
+++ Sfml/Window/Properties/AssemblyInfo.cs
@@ -1,20 +1,20 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("Pulsar Packager")]
-[assembly: AssemblyDescription ("Pulsar Packager")]
+[assembly: AssemblyTitle ("sfml-window")]
+[assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("Pulsar Packager")]
-[assembly: AssemblyCopyright ("Copyright © Sylvain PONTOREAU 2014")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("2.1.*")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
