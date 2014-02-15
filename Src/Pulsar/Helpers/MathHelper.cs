307be2fd78741f42d242db8d7587834005eb643c
pontoreau <pontoreau.sylvain@gmail.com>
2014-02-15 21:41:42 +0100

    MathHelper revision + unit test

--- Src/Pulsar/Helpers/MathHelper.cs
+++ Src/Pulsar/Helpers/MathHelper.cs
@@ -1,235 +1,192 @@
-/* License
- * 
- * The MIT License (MIT)
- *
- * Copyright (c) 2014, Sylvain PONTOREAU (pontoreau.sylvain@gmail.com)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 using System;
 
 namespace Pulsar.Helpers
 {
 	/// <summary>
-	/// Extensions of the Math class.
+	/// Math helper.
 	/// </summary>
 	public static class MathHelper
 	{
 		/// <summary>
-		/// Pi contant
+		/// The pi.
 		/// </summary>
 		public const float Pi = (float)Math.PI;
 
 		/// <summary>
-		/// 2 PI constant.
+		/// The two pi.
 		/// </summary>
-		public const float TwoPi = (float)(Math.PI * 2);
+		public const float TwoPi = Pi * 2;
 
 		/// <summary>
-		/// E constant.
+		/// The e.
 		/// </summary>
 		public const float E = (float)(Math.E);
 
 		/// <summary>
-		/// Get the absolute value of a float.
+		/// Absolute the specified value.
 		/// </summary>
-		/// <param name="value">The absolute value of a float.</param>
-		/// <returns>The absolute value of the float.</returns>
+		/// <param name="value">Value.</param>
 		public static float Abs(float value)
 		{
 			return Math.Abs(value);
 		}
 
 		/// <summary>
-		/// get the Square root value of a float.
+		/// Square root the specified value.
 		/// </summary>
-		/// <param name="value">The value.</param>
-		/// <returns>The square root value of a float.</returns>
+		/// <param name="value">Value.</param>
 		public static float Sqrt(float value)
 		{
-			return (float)Math.Sqrt((double)value);
+			return (float)Math.Sqrt(value);
 		}
 
 		/// <summary>
-		/// Get the value pow 2
+		/// Square the specified value.
 		/// </summary>
-		/// <param name="value">Value</param>
-		/// <returns>The value over 2</returns>
+		/// <param name="value">Value.</param>
 		public static float Square(float value)
 		{
 			return value * value;
 		}
 
 		/// <summary>
-		/// get a value pow by an other value.
+		/// Pow the specified x by y.
 		/// </summary>
-		/// <param name="x">X value.</param>
-		/// <param name="y">Pow value.</param>
-		/// <returns></returns>
+		/// <param name="x">The x coordinate.</param>
+		/// <param name="y">The y coordinate.</param>
 		public static float Pow(float x, float y)
 		{
-			return (float)Math.Pow((double)x, (double)y);
+			return (float)Math.Pow(x, y);
 		}
 
 		/// <summary>
-		/// Get the angular representation of a cosinus value.
+		/// Acos the specified value.
 		/// </summary>
-		/// <param name="value">Cosinus value.</param>
-		/// <returns>Angle corresponding to the cosinus.</returns>
+		/// <param name="value">Value.</param>
 		public static float Acos(float value)
 		{
-			return (float)Math.Acos((double)value);
+			return (float)Math.Acos(value);
 		}
 
 		/// <summary>
-		/// Get the angular representation of a sinus value.
+		/// Asin the specified value.
 		/// </summary>
-		/// <param name="value">Sinus value.</param>
-		/// <returns>Angle corresponding to the sinus.</returns>
+		/// <param name="value">Value.</param>
 		public static float Asin(float value)
 		{
-			return (float)Math.Asin((double)value);
+			return (float)Math.Asin(value);
 		}
 
 		/// <summary>
-		/// Get the angular representation of a tangent value.
+		/// Atan the specified value.
 		/// </summary>
-		/// <param name="value">Tangent value.</param>
-		/// <returns>Angle corresponding to the tangent.</returns>
+		/// <param name="value">Value.</param>
 		public static float Atan(float value)
 		{
-			return (float)Math.Atan((double)value);
+			return (float)Math.Atan(value);
 		}
 
 		/// <summary>
-		/// Return the arctangent of quotient of y and x
+		/// Atan2 the specified y and x.
 		/// </summary>
-		/// <param name="y">X coordinate</param>
-		/// <param name="x">Y coordinate</param>
-		/// <returns>Arctangent corresponding</returns>
+		/// <param name="y">The y coordinate.</param>
+		/// <param name="x">The x coordinate.</param>
 		public static float Atan2(float y, float x)
 		{
-			return (float)Math.Atan2((double)y, (double)x);
+			return (float)Math.Atan2(y, x);
 		}
 
 		/// <summary>
-		/// Get the sinus value of an angle.
+		/// Sin the specified value.
 		/// </summary>
-		/// <param name="value">The angle value.</param>
-		/// <returns>Sinus corresponding to the angle value.</returns>
+		/// <param name="value">Value.</param>
 		public static float Sin(float value)
 		{
-			return (float)Math.Sin((double)value);
+			return (float)Math.Sin(value);
 		}
 
 		/// <summary>
-		/// Get the tangent value of an angle.
+		/// Tan the specified value.
 		/// </summary>
-		/// <param name="value">The angle value.</param>
-		/// <returns>Tangent corresponding to the angle value.</returns>
+		/// <param name="value">Value.</param>
 		public static float Tan(float value)
 		{
-			return (float)Math.Tan((double)value);
+			return (float)Math.Tan(value);
 		}
 
 		/// <summary>
-		/// Get the cosinus value of an angle.
+		/// Cos the specified value.
 		/// </summary>
-		/// <param name="value">The angle value.</param>
-		/// <returns>Cosinus corresponding to the angle value.</returns>
+		/// <param name="value">Value.</param>
 		public static float Cos(float value)
 		{
-			return (float)Math.Cos((double)value);
+			return (float)Math.Cos(value);
 		}
 
 		/// <summary>
-		/// Get E pow value.
+		/// E pow the specified value.
 		/// </summary>
-		/// <param name="value">Pow value.</param>
-		/// <returns>E pow value.</returns>
+		/// <param name="value">Value.</param>
 		public static float Exp(float value)
 		{
-			return (float)Math.Exp((double)value);
+			return (float)Math.Exp(value);
 		}
 
 		/// <summary>
-		/// get the Log E base of a value.
+		/// Log E the specified value.
 		/// </summary>
-		/// <param name="value">The value.</param>
-		/// <returns>Log E base a the value.</returns>
+		/// <param name="value">Value.</param>
 		public static float Log(float value)
 		{
-			return (float)Math.Log((double)value);
+			return (float)Math.Log(value);
 		}
 
 		/// <summary>
-		/// get the Log base 10 of a value.
+		/// Log 10 the specified value.
 		/// </summary>
-		/// <param name="value">The value.</param>
-		/// <returns>Log base 10 a the value.</returns>
+		/// <param name="value">Value.</param>
 		public static float Log10(float value)
 		{
-			return (float)Math.Log10((double)value);
+			return (float)Math.Log10(value);
 		}
 
 		/// <summary>
-		/// Linearly interpolates between two values. Passing amount a value of 0 will cause value1 to be returned, a value of 1 will cause value2 to be returned.
+		/// Linear interpolate the specified value1 and value2 by amount.
 		/// </summary>
-		/// <param name="value1">Source value.</param>
-		/// <param name="value2">Source value.</param>
-		/// <param name="amount">Value between 0 and 1 indicating the weight of value2.</param>
-		/// <returns>Lerp</returns>
+		/// <param name="value1">Value1.</param>
+		/// <param name="value2">Value2.</param>
+		/// <param name="amount">Amount.</param>
 		public static float Lerp(float value1, float value2, float amount)
 		{
 			return value1 + (value2 - value1) * amount;
 		}
 
 		/// <summary>
-		/// Returns the greater of two values.
+		/// Max value between value1 and value2.
 		/// </summary>
-		/// <param name="value1">Source value.</param>
-		/// <param name="value2">Source value.</param>
-		/// <returns>The greater value.</returns>
+		/// <param name="value1">Value1.</param>
+		/// <param name="value2">Value2.</param>
 		public static float Max(float value1, float value2)
 		{
 			return (value1 > value2) ? value1 : value2;
 		}
 
 		/// <summary>
-		/// Returns the lesser of two values.
+		/// Minimum value between value1 and value2.
 		/// </summary>
-		/// <param name="value1">Source value.</param>
-		/// <param name="value2">Source value.</param>
-		/// <returns>The lesser value.</returns>
+		/// <param name="value1">Value1.</param>
+		/// <param name="value2">Value2.</param>
 		public static float Min(float value1, float value2)
 		{
 			return (value1 < value2) ? value1 : value2;
 		}
 
 		/// <summary>
-		/// Restricts a value to be within a specified range.
+		/// Clamp the specified value.
 		/// </summary>
-		/// <param name="value">The value to clamp.</param>
-		/// <param name="min">The minimum value.</param>
-		/// <param name="max">The maximum value.</param>
-		/// <returns>The clamped value.</returns>
+		/// <param name="value">Value.</param>
+		/// <param name="min">Minimum.</param>
+		/// <param name="max">Max.</param>
 		public static float Clamp(float value, float min, float max)
 		{
 			if (value > max)
@@ -241,90 +198,85 @@ namespace Pulsar.Helpers
 		}
 
 		/// <summary>
-		/// Round a float value
+		/// Round the specified f.
 		/// </summary>
-		/// <param name="f">The float value</param>
-		/// <returns>The float round value</returns>
+		/// <param name="f">F.</param>
 		public static float Round(float f)
 		{
-			return (float)Math.Round((double)f);
+			return (float)Math.Round(f);
 		}
 
 		/// <summary>
-		/// Round a float value
+		/// Round the specified f and i.
 		/// </summary>
-		/// <param name="f">The float value</param>
-		/// <param name="i">The int value after comma</param>
-		/// <returns></returns>
+		/// <param name="f">F.</param>
+		/// <param name="i">decimal next coma to round.</param>
 		public static float Round(float f, int i)
 		{
 			return (float)Math.Round(f, i);
 		}
 
 		/// <summary>
-		/// Return distance betwen two point coordinates
+		/// Distance between point 1 and 2.
 		/// </summary>
-		/// <param name="x1">point 1 X coordinate.</param>
-		/// <param name="y1">point 1 Y coordinate.</param>
-		/// <param name="x2">point 2 X coordinate.</param>
-		/// <param name="y2">point 2 Y coordinate.</param>
-		/// <returns>Distance between the Point coordinates 1 and 2.</returns>
+		/// <param name="x1">The first x value.</param>
+		/// <param name="y1">The first y value.</param>
+		/// <param name="x2">The second x value.</param>
+		/// <param name="y2">The second y value.</param>
 		public static float Distance(float x1, float y1, float x2, float y2)
 		{
 			return Sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
 		}
 
 		/// <summary>
-		/// Return angle of two point coordinates in radians.
+		/// Radians angle between point 1 and 2.
 		/// </summary>
-		/// <param name="x1">point 1 X coordinate.</param>
-		/// <param name="y1">point 1 Y coordinate.</param>
-		/// <param name="x2">point 2 X coordinate.</param>
-		/// <param name="y2">point 2 Y coordinate.</param>
-		/// <returns>Angle in radian of of the point coordinates 1 and 2.</returns>
+		/// <returns>The angle.</returns>
+		/// <param name="x1">The first x value.</param>
+		/// <param name="y1">The first y value.</param>
+		/// <param name="x2">The second x value.</param>
+		/// <param name="y2">The second y value.</param>
 		public static float RadiansAngle(float x1, float y1, float x2, float y2)
 		{
 			return Atan2(x1 - x2, y2 - y1);
 		}
 
 		/// <summary>
-		/// Convert radian angle into degree angle.
+		/// Convert radians to the degrees.
 		/// </summary>
-		/// <param name="radian">Radian angle.</param>
-		/// <returns>Degree angle.</returns>
+		/// <returns>The degrees.</returns>
+		/// <param name="radians">Radians.</param>
 		public static float ToDegrees(float radians)
 		{
 			return radians * (180.0f / Pi);
 		}
 
 		/// <summary>
-		/// Convert degree angle into radian angle.
+		/// Convert radians to degrees.
 		/// </summary>
-		/// <param name="degree">Degree angle.</param>
-		/// <returns>Radian angle.</returns>
+		/// <returns>The radians.</returns>
+		/// <param name="degrees">Degrees.</param>
 		public static float ToRadians(float degrees)
 		{
 			return degrees * (Pi / 180.0f);
 		}
 
 		/// <summary>
-		/// Return true if clockwise direction is the faster way to go from current angle to reach angle.
+		/// ClockWise direction.
 		/// </summary>
-		/// <param name="currentAngle">Current angle</param>
-		/// <param name="angleToReach">Angle to reach</param>
-		/// <returns>True if clockwise direction is the faster way to go from current angle to reach angle.</returns>
+		/// <returns><c>true</c>, if ClockWise, <c>false</c> otherwise.</returns>
+		/// <param name="currentAngle">Current angle.</param>
+		/// <param name="angleToReach">Angle to reach.</param>
 		public static bool ClockWiseDirection(float currentAngle, float angleToReach)
 		{
 			var clockWiseDirection = angleToReach - currentAngle;
 
-			var isClockWiseDirection = true;
+			if (clockWiseDirection > 0f && Abs(clockWiseDirection) <= 180f) return true;
+			if (clockWiseDirection > 0f && Abs(clockWiseDirection) > 180f) return false;
+			if (clockWiseDirection < 0f && Abs(clockWiseDirection) <= 180f) return false;
+			if (clockWiseDirection < 0f && Abs(clockWiseDirection) > 180f) return true;
 
-			if (clockWiseDirection > 0 && Abs(clockWiseDirection) <= 180) isClockWiseDirection = true;
-			else if (clockWiseDirection > 0 && Abs(clockWiseDirection) > 180) isClockWiseDirection = false;
-			else if (clockWiseDirection < 0 && Abs(clockWiseDirection) <= 180) isClockWiseDirection = false;
-			else if (clockWiseDirection < 0 && Abs(clockWiseDirection) > 180) isClockWiseDirection = true;
-
-			return isClockWiseDirection;
+			return true;
 		}
 	}
 }
---
+++ Test/Pulsar.Test/Helpers/MathHelperTest.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using NUnit.Framework;
+using Pulsar.Helpers;
+
+namespace Pulsar.Test
+{
+	[TestFixture]
+	public class MathHelperTest
+	{
+		[Test]
+		public void TestPi()
+		{
+			var actual = MathHelper.Pi;
+			var expected = (float)Math.PI;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestTwoPi()
+		{
+			var actual = MathHelper.TwoPi;
+			var expected = (float)Math.PI * 2;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestE()
+		{
+			var actual = MathHelper.E;
+			var expected = (float)Math.E;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestAbs()
+		{
+			var actual = MathHelper.Abs(-1f);
+			var expected = 1f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestSqrt()
+		{
+			var actual = MathHelper.Sqrt(9f);
+			var expected = 3f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestSquare()
+		{
+			var actual = MathHelper.Square(3f);
+			var expected = 9f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestPow()
+		{
+			var actual = MathHelper.Pow(3f, 2f);
+			var expected = 9f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestAcos()
+		{
+			var actual = MathHelper.Round(MathHelper.Acos(0f), 6);
+			var expected = 1.570796f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestAsin()
+		{
+			var actual = MathHelper.Round(MathHelper.Asin(1f), 6);
+			var expected = 1.570796f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestAtan()
+		{
+			var actual = MathHelper.Round(MathHelper.Atan(1f), 6);
+			var expected = 0.785398f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestAtan2()
+		{
+			var actual = MathHelper.Round(MathHelper.Atan2(1f, 1f), 6);
+			var expected = 0.785398f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestCos()
+		{
+			var actual = MathHelper.Round(MathHelper.Cos(MathHelper.Pi / 2f), 6);
+			var expected = 0f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestSin()
+		{
+			var actual = MathHelper.Round(MathHelper.Sin(MathHelper.Pi / 2f), 6);
+			var expected = 1f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestTan()
+		{
+			var actual = MathHelper.Round(MathHelper.Tan(MathHelper.Pi / 4f), 6);
+			var expected = 1f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestExp()
+		{
+			var actual = MathHelper.Round(MathHelper.Exp(1.0f), 6);
+			var expected = MathHelper.Round(MathHelper.E, 6);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestLog()
+		{
+			var actual = MathHelper.Round(MathHelper.Log(MathHelper.E), 6);
+			var expected = 1f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestLog10()
+		{
+			var actual = MathHelper.Log10(10f);
+			var expected = 1f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestLerp()
+		{
+			var actual = MathHelper.Lerp(1f, 2f, 0.5f);
+			var expected = 1.5f;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestClamp()
+		{
+			var expected = 3f;
+			var expectedMin = 4f;
+			var expectedMax = 2f;
+			var actual = MathHelper.Clamp (expected, 2f, 4f);
+			var actualMin = MathHelper.Clamp (expected, 4f, 5f);
+			var actualMax = MathHelper.Clamp (expected, 1f, 2f);
+
+			Assert.AreEqual (expected, actual);
+			Assert.AreEqual (expectedMin, actualMin);
+			Assert.AreEqual (expectedMax, actualMax);
+		}
+
+		[Test]
+		public void TestMax()
+		{
+			var expected = 3f;
+			var actual = MathHelper.Max(1f, 3f);
+
+			Assert.AreEqual (expected, actual);
+		}
+
+		[Test]
+		public void TestMin()
+		{
+			var expected = 1f;
+			var actual = MathHelper.Min(1f, 3f);
+
+			Assert.AreEqual (expected, actual);
+		}
+
+		[Test]
+		public void TestRound()
+		{
+			var expectedMin = 1f;
+			var expectedMax = 2f;
+			var expectedMiddle = 2f;
+
+			var actualMin = MathHelper.Round (1.2f);
+			var actualMax = MathHelper.Round (1.7f);
+			var actualMiddle = MathHelper.Round (1.5f);
+
+			Assert.AreEqual (expectedMin, actualMin);
+			Assert.AreEqual (expectedMax, actualMax);
+			Assert.AreEqual (expectedMiddle, actualMiddle);
+		}
+
+		[Test]
+		public void TestDistance()
+		{
+			var expectedX = 1f;
+			var actualX = MathHelper.Distance (1f, 0f, 2f, 0f);
+
+			var expectedY = 1f;
+			var actualY = MathHelper.Distance (0f, 1f, 0f, 2f);
+
+			Assert.AreEqual (expectedY, actualY);
+			Assert.AreEqual (expectedX, actualX);
+		}
+
+		[Test]
+		public void TestRadiansAngle()
+		{
+			var expected = MathHelper.Pi;
+			var actual = MathHelper.RadiansAngle(0f, 0f, 0f, -1f);
+
+			Assert.AreEqual (expected, actual);
+		}
+
+		[Test]
+		public void TestToDegrees()
+		{
+			var expected = 180f;
+			var actual = MathHelper.ToDegrees (MathHelper.Pi);
+
+			Assert.AreEqual (expected, actual);
+		}
+
+		[Test]
+		public void TestToRadians()
+		{
+			var expected = MathHelper.Pi;
+			var actual = MathHelper.ToRadians (180f);
+
+			Assert.AreEqual (expected, actual);
+		}
+
+		[Test]
+		public void TestClockWiseDirection()
+		{
+			var clockWise = MathHelper.ClockWiseDirection (90f, 180f);
+			var unClockWise = MathHelper.ClockWiseDirection (180f, 90f);
+
+			Assert.IsTrue (clockWise);
+			Assert.IsFalse (unClockWise);
+		}
+	}
+}
+
