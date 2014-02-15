209bf87c07397875073bfda12aeacce3b17a2550
pontoreau <pontoreau.sylvain@gmail.com>
2014-02-15 17:55:44 +0100

    Edit Sfml project file and add .config for DllMap Add samples project

---
+++ Samples/PulsarContent/GameTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Pulsar;
+using SFML.Graphics;
+
+namespace PulsarContent
+{
+	public class GameTest : Game
+	{
+		public GameTest ()
+			:base()
+		{
+			Content.RootDirectory = "Content";
+		}
+
+		protected override void LoadContent ()
+		{
+			var texture = Content.Load<Texture> ("pulsar.png");
+
+
+			base.LoadContent ();
+		}
+
+		protected override void Draw (GameTime gameTime)
+		{
+			base.Draw (gameTime);
+		}
+
+	}
+}
+
---
+++ Samples/PulsarContent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PulsarContent
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var game = new GameTest();
+			game.Run();
+		}
+	}
+}
